374dbf573564a4bb031800f24e86cf92eb69b4f9
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-10-06 15:49:23 +0100

    ch: Day2 completed

--- Day2/exercise/main.js
+++ Day2/exercise/main.js
@@ -93,8 +93,7 @@ console.log(twister.replace(because, ''))
 
 //LEVEL 3
 let loveSentence = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
-console.log(loveSentence.match(/love/gi).length
-)
+console.log(loveSentence.match(/love/gi).length)
 console.log(twister.match(/because/gi).length)
 
 const sentence2 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
