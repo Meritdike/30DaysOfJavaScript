b3efdbf5f4ddc9341c0536813d4879ed798a9fe3
Merit Dike <65596735+Meritdike@users.noreply.github.com>
2022-10-29 18:33:45 +0100

    ch: Day7 level 1 exercises completed

--- Day7/exercise/main.js
+++ Day7/exercise/main.js
@@ -52,3 +52,71 @@ console.log(density(10, 7))
 const speed = (d,t) => d/t + 'm/s'
 console.log(speed(2,4))
 
+const weight = (m, g) =>  m*g + 'kg'
+console.log(weight(7, 9))
+
+const convertCelsiusToFahrenheit = (oC) => (oC * 9/5) + 32 + 'oF'
+console.log(convertCelsiusToFahrenheit(56))
+
+function BMI(m, h) {
+    let bmi = (m / Math.pow(h, 2)).toFixed(2)
+    if (bmi < 18.5) return `You are Underweight with ${bmi}kg/m^2`
+    else if (bmi > 18.5 && bmi <= 24.9) return `You are Normal weight with ${bmi}kg/m^2`
+    else if (bmi >= 25 && bmi <= 29.9) return `You are Overweight with ${bmi}kg/m^2`
+    else if(bmi >= 30) return `You are Obese with ${bmi}kg/m^2`
+    else return 'Input a correct parameter'
+}
+console.log(BMI(66, 1.73));
+
+function checkMonth(monthCheck) {
+    let month = monthCheck[0].toUpperCase() + monthCheck.slice(1).toLowerCase()
+    switch (month) {
+        case 'January':
+        case 'March':
+        case 'May':
+        case 'July':
+        case 'August':
+        case 'October':
+        case 'December':
+            return`${month} has 31 days.`
+        case 'September':
+        case 'April':
+        case 'June':
+        case 'November':
+            return`${month} has 30 days.`
+        case 'February':
+            return`${month} has 29 days`
+        default:
+            return`${month} is not a month of the year`
+    }
+}
+console.log(checkMonth('JUNE'));
+
+function checkSeason(monthCheck) {
+    let month = monthCheck[0].toUpperCase() + monthCheck.slice(1).toLowerCase()
+    switch (month) {
+        case 'January':
+        case 'February':
+        case 'December':
+            return 'Winter'
+        case 'March':
+        case 'April':
+        case 'May':
+            return 'Spring'
+        case 'June':
+        case 'July':
+        case 'August':
+            return 'Summer'
+        case 'September':
+        case 'October':
+        case 'November':
+            return 'Autumn'
+        default:
+            return `${month} is not a month of the year`
+    }
+}
+console.log(checkSeason('JUNe'));
+
+const findMax = (a, b, c) => Math.max(a,b,c)
+console.log(findMax(0, 10, 5))
+console.log(findMax(0, -10, -2))
